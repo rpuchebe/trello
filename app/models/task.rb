be0b3a5733548a503d6b69dde0320837152afcdb
Raimundo Puche <rey_puche_884@hotmail.com>
2015-04-29 03:36:03 +0000

    Devise

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ActiveRecord::Base
 
 	belongs_to :user
+	validates :tittle , presence: true 
+	validates :description , presence: true 
+	validates :start_time , presence: true 
+	validates :finish_time , presence: true 
+	validates :duration , presence: true 
+	    
+
 end
